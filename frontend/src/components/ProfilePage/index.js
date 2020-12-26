780dd8f5538412c76bd4a9a61bedf28de1efcf1b
Mustafa <mustafaomousa@icloud.com>
2020-12-26 14:40:01 -0600

    I edited my profile component to render the users info and added a couple of buttons.

--- frontend/src/components/ProfilePage/index.js
+++ frontend/src/components/ProfilePage/index.js
@@ -1,10 +1,14 @@
+import { useContext } from 'react';
 import { useDispatch } from 'react-redux';
 import { useHistory } from 'react-router-dom';
 import * as sessionActions from '../../store/session';
+import { AppContext } from '../../context/AppContextProvider';
 
 const ProfilePage = () => {
     const dispatch = useDispatch();
     const history = useHistory();
+
+    const { user } = useContext(AppContext);
 
     const logout = (e) => {
         e.preventDefault();
@@ -13,13 +17,20 @@ const ProfilePage = () => {
     };
 
     return (
-        <ul className="profile-dropdown">
-            <li>Username here</li>
-            <li>Password here</li>
-            <li>
+        <>
+            <h1>Profile</h1>
+            <ul className="profile-dropdown">
+                <li>Username: {user.username}</li>
+                <li>Email: {user.email}</li>
+                <li>Phone Number: {user.phoneNumber}</li>
+            </ul>
+            <div className='profile-button-box'>
                 <button onClick={logout}>Log Out</button>
-            </li>
-        </ul>
+                <button onClick={logout}>Delete Account</button>
+                <button onClick={logout}>Edit Profile Information</button>
+                <button onClick={logout}>Change Password</button>
+            </div>
+        </>
     )
 };
 
