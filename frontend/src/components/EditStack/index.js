b2edb304ce29f06461b4433dfbfea20f5ac151f0
Mustafa <mustafaomousa@icloud.com>
2020-12-30 12:03:52 -0600

    added some new edit features

--- frontend/src/components/EditStack/index.js
+++ frontend/src/components/EditStack/index.js
@@ -14,6 +14,8 @@ const EditStack = () => {
     const cards = useSelector(state => state.card.cards);
 
     const [newCardActive, setNewCardActive] = useState(false);
+    const [editCardActive, setEditCardActive] = useState(false);
+    const [selectedCard, setSelectedCard] = useState();
     const [front, setFront] = useState('');
     const [back, setBack] = useState('');
 
@@ -25,25 +27,60 @@ const EditStack = () => {
         setNewCardActive(true);
     }
 
+    const editCard = (e) => {
+        e.preventDefault();
+        setEditCardActive(true);
+    }
+
     const submitNewCard = (e) => {
         e.preventDefault();
         dispatch(createCard(paramsId, front, back));
         setNewCardActive(false);
     }
 
+    const submitEditCard = (e) => {
+        e.preventDefault();
+        alert('feature coming soon')
+        setEditCardActive(false);
+    }
+
+    const selectCard = (e, cardId) => {
+        e.preventDefault();
+        console.log(cardId)
+        setSelectedCard(cards.find(card => card.id === cardId));
+    };
+
     const editFields = () => {
         return (
-            <form onSubmit={submitNewCard} className='edit-stage'>
-                <label>Front:
-                                        <input onChange={updateFront} value={front}></input>
-                </label>
-                <label>Back:
-                                        <input onChange={updateBack} value={back}></input>
-                </label>
-                <button id='special-button'>Create</button>
-            </form>
+            <div>
+                <form onSubmit={() => alert('feature coming soon')} className='edit-stage'>
+                    <label>Front:
+                                        <input onChange={updateFront}></input>
+                    </label>
+                    <label>Back:
+                                        <input onChange={updateBack}></input>
+                    </label>
+                    <button id='special-button'>Edit</button>
+                </form>
+            </div>
         )
     };
+
+    const newCardFields = () => {
+        return (
+            <div>
+                <form onSubmit={submitNewCard} className='edit-stage'>
+                    <label>Front:
+                                        <input onChange={updateFront} value={front}></input>
+                    </label>
+                    <label>Back:
+                                        <input onChange={updateBack} value={back}></input>
+                    </label>
+                    <button id='special-button'>Create</button>
+                </form>
+            </div>
+        )
+    }
 
     useEffect(() => {
         dispatch(getStacks());
@@ -53,15 +90,22 @@ const EditStack = () => {
         <>
             { stack && (
                 <div className='edit-stack-body'>
-                    <div className='edit-stack-control-container'>
-                        <div className='control-panel'>
-                            <button id='special-button'>Edit card</button>
-                            <button id='special-button'>Delete card</button>
-                        </div>
-                        <div>
-                            Render inputs using ajax here to change card info
-                        </div>
-                    </div>
+                    {selectedCard && (
+                        <div className='edit-stack-control-container'>
+                            <div className='control-panel'>
+                                <button onClick={editCard} id='special-button'>Edit card</button>
+                                <button id='special-button'>Delete card</button>
+                            </div>
+                            {editCardActive && editFields()}
+                            <div className='selected-card-div'>
+                                <div className='card'>
+                                    <p style={{ fontWeight: 'bolder' }}>{selectedCard.term}</p>
+                                </div>
+                                <div className='card'>
+                                    <p>{selectedCard.response}</p>
+                                </div>
+                            </div>
+                        </div>)}
                     <div className='stack-card'>
                         <div className='stack-card-container'>
                             <h2>Stack name: {stack.name}</h2>
@@ -71,12 +115,12 @@ const EditStack = () => {
                                 <button id='special-button'>Delete stack</button>
                             </div>
                             <div className='edit'>
-                                {newCardActive && editFields()}
+                                {newCardActive && newCardFields()}
                             </div>
                             <div className='cards-container'>
                                 {cards.map((card, i) => {
                                     return (
-                                        <div className='card'>
+                                        <div onClick={(e) => selectCard(e, card.id)} key={`card-${i}`} className={`card`} value={card.id} >
                                             <h3>{card.term}</h3>
                                         </div>
                                     )
