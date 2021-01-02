fcc331e425903b343241b3932f51a37ceab39e33
Mustafa <mustafaomousa@icloud.com>
2021-01-02 14:27:18 -0600

    made the play button functional

--- frontend/src/components/Stack/index.js
+++ frontend/src/components/Stack/index.js
@@ -4,18 +4,21 @@ import './stack.css';
 
 const Stack = ({ stack }) => {
     const [studyEnabled, setStudyEnabled] = useState(false);
+    const [isFlipped, setIsFlipped] = useState(false);
 
     return (
         <div className='single-stack-body'>
             <div className='single-stack-control'>
                 <h2>Control Panel</h2>
-                <div className='how-to-play'>
+                <div className={studyEnabled ? 'how-to-disabled' : 'how-to-play'}>
                     <h4>How to play:</h4>
                     <p>To begin studying the stack select 'Play' below.</p>
+                    <button onClick={() => setStudyEnabled(true)}>Play</button>
                 </div>
-                <button onClick={() => setStudyEnabled(!studyEnabled)}>Play</button>
-                <div className='control-panel-buttons'>
-
+                <div className={studyEnabled ? 'study-panel-buttons' : 'how-to-disabled'}>
+                    <button>Prev</button>
+                    <button onClick={() => setStudyEnabled(false)} > End</button>
+                    <button>Next</button>
                 </div>
             </div>
             <div className={!studyEnabled ? 'stack-container' : 'stack-study-container-disabled'}>
@@ -27,7 +30,12 @@ const Stack = ({ stack }) => {
             </div>
             <div className='stack-study-container' className={studyEnabled ? 'stack-study-container-enabled' : 'stack-study-container-disabled'}>
                 <div className={`single-stack`} hidden='true'>
-
+                    <div className={isFlipped ? 'card-disabled' : 'front-card'}>
+                        <p>Front of the card</p>
+                    </div>
+                    <div className={isFlipped ? 'front-card' : 'card-disabled'}>
+                        <p>Back of the card</p>
+                    </div>
                 </div>
             </div>
         </div>
