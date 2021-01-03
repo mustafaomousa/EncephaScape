65bc9be505da8abf4e7f36e4629ec529cc9342bd
Mustafa <mustafaomousa@icloud.com>
2021-01-03 16:27:20 -0600

    made logout and delete account more user friendly

--- frontend/src/components/ProfilePage/index.js
+++ frontend/src/components/ProfilePage/index.js
@@ -1,4 +1,4 @@
-import { useContext } from 'react';
+import { useContext, useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { Redirect, useHistory } from 'react-router-dom';
 import * as sessionActions from '../../store/session';
@@ -10,18 +10,32 @@ const ProfilePage = () => {
     const dispatch = useDispatch();
     const history = useHistory();
 
+    const [showDelete, setShowDelete] = useState(true);
+    const [showLogout, setShowLogout] = useState(true);
+
     const { user } = useContext(AppContext);
     const sessionUser = useSelector((state) => state.session.user);
 
-    if (sessionUser === undefined) {
-        alert('Please login or sign-up');
-        return <Redirect to='/signup'></Redirect>
-    };
+
 
     const logout = (e) => {
         e.preventDefault();
         dispatch(sessionActions.logout());
         history.push('/');
+    };
+
+    const restore = (e) => {
+        setShowDelete(true);
+        setShowLogout(true);
+    };
+
+    useEffect(() => {
+
+    }, [showDelete, showLogout])
+
+    if (sessionUser === undefined) {
+        alert('Please login or sign-up');
+        return <Redirect to='/signup'></Redirect>
     };
 
     return (
@@ -42,10 +56,29 @@ const ProfilePage = () => {
                 </div>
             </div>
             <div className='profile-button-box'>
-                <button onClick={logout}>Log Out</button>
-                <button onClick={logout}>Delete Account</button>
+                <button onClick={() => setShowLogout(false)}>Log Out</button>
+                <button onClick={() => setShowDelete(false)}>Delete Account</button>
+
                 <button onClick={logout}>Edit Profile Information</button>
                 <button onClick={logout}>Change Password</button>
+            </div>
+            <div className='delete-account'>
+                <div className={showDelete ? 'hidden' : 'delete-account-buttons'}>
+                    <div>
+                        <p>Are you sure you want to delete your account?</p>
+                    </div>
+                    <button>Yes</button>
+                    <button onClick={restore}>No</button>
+                </div>
+            </div>
+            <div className='logout-account'>
+                <div className={showLogout ? 'hidden' : 'delete-account-buttons'}>
+                    <div>
+                        <p>Are you sure you want to logout?</p>
+                    </div>
+                    <button onClick={logout}>Yes</button>
+                    <button onClick={restore}>No</button>
+                </div>
             </div>
         </div>
     )
