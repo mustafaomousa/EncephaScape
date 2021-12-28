0b1f9aa5817ae19f8d0d702b788fdc86b1924925
mustafaomousa <mustafaomousa@icloud.com>
2021-12-28 11:26:39 -0600

    redesigned the home page

--- frontend/src/components/HomePage/index.js
+++ frontend/src/components/HomePage/index.js
@@ -1,41 +1,23 @@
 import { useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { NavLink } from "react-router-dom";
+import { Box, Button, Grid, Stack, Typography } from "@mui/material";
+import { makeStyles } from "@mui/styles";
 import { getAllCategories } from "../../store/category";
 import { getStacks, getTopStacks } from "../../store/stack";
-import "./homepage.css";
+
+const useStyles = makeStyles(() => ({
+  root: {
+    height: "100vh",
+    backgroundColor: "#333333",
+  },
+}));
 
 const HomePage = () => {
+  const classes = useStyles();
   const dispatch = useDispatch();
   const categories = useSelector((state) => state.category.categories);
   const newStacks = useSelector((state) => state.stack.newestStacks);
-
-  const categoryItems = categories.map((category) => {
-    return (
-      <ul
-        className={`category category-${category.id}`}
-        key={`category-${category.id}`}
-      >
-        <NavLink
-          className="category-nav-link"
-          key={`${category.id}`}
-          to={`/category/${category.id}`}
-        >
-          {category.name}
-        </NavLink>
-      </ul>
-    );
-  });
-
-  const newestStacks = newStacks.map((stack, i) => {
-    return (
-      <div key={`category-${stack.id}`} className="newest-stack">
-        <NavLink className="stack" to={`/stack/${stack.id}`}>
-          {stack.name}
-        </NavLink>
-      </div>
-    );
-  });
 
   useEffect(() => {
     dispatch(getAllCategories());
@@ -44,22 +26,54 @@ const HomePage = () => {
   }, [dispatch]);
 
   return (
-    <div className="home-body">
-      <div className="welcome">
-        <h1>Welcome</h1>
-        <br />
-        <p>
-          The beginning of our name "Encepha" derives from the Greek word
-          "Encephalon" meaning "brain". Here at EncephaScape we believe in the
-          wonders that our brains can perform through studying. Join today to
-          access tens of our members stacks and to create your own 'Brainfolio!'
-        </p>
-      </div>
-      <h4>browse our members newest stacks...</h4>
-      <div className="recent-stacks">{newestStacks}</div>
-      <h4>browse our stack categories</h4>
-      <div className="categories">{categoryItems}</div>
-    </div>
+    <Box className={classes.root}>
+      <Grid container height="100%" padding={10}>
+        <Grid item sm={12} md={6}>
+          <Stack
+            height="100%"
+            width="100%"
+            alignItems="flex-end"
+            justifyContent="center"
+            spacing={1}
+          >
+            <Typography variant="h3" textAlign="end" color="white">
+              We believe in the wonders our brain can perform through studying
+            </Typography>
+            <Grid container justifyContent={"flex-end"} spacing={2}>
+              <Grid item>
+                <Button
+                  disableElevation
+                  fullWidth
+                  variant="contained"
+                  size="large"
+                >
+                  Search stacks
+                </Button>
+              </Grid>
+              <Grid item>
+                <Button
+                  disableElevation
+                  fullWidth
+                  variant="contained"
+                  size="large"
+                >
+                  Create a stack
+                </Button>
+              </Grid>
+            </Grid>
+          </Stack>
+        </Grid>
+        <Grid item sm={12} md={6}>
+          <Stack
+            height="100%"
+            width="100%"
+            alignItems="flex-end"
+            justifyContent="center"
+            spacing={1}
+          ></Stack>
+        </Grid>
+      </Grid>
+    </Box>
   );
 };
 
--- frontend/src/components/Navigation/index.js
+++ frontend/src/components/Navigation/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Button, Divider, Grid, Link, Stack, Typography } from "@mui/material";
+import { AppBar, Button, Grid, Link, Stack, Typography } from "@mui/material";
 import { makeStyles } from "@mui/styles";
 import { useSelector } from "react-redux";
 import brain from "./brain.png";
@@ -20,64 +20,66 @@ function Navigation() {
   const classes = useStyles();
 
   return (
-    <Grid container className={classes.root}>
-      <Grid item xs={6}>
-        <Stack alignItems="flex-start" justifyContent="center" height="100%">
-          <img
-            src={brain}
-            className={classes.logoImage}
-            alt="EncephaScape"
-          ></img>
-        </Stack>
+    <AppBar elevation={0}>
+      <Grid container className={classes.root}>
+        <Grid item xs={6}>
+          <Stack alignItems="flex-start" justifyContent="center" height="100%">
+            <img
+              src={brain}
+              className={classes.logoImage}
+              alt="EncephaScape"
+            ></img>
+          </Stack>
+        </Grid>
+        <Grid item xs={6}>
+          <Stack
+            direction="row"
+            alignItems="center"
+            justifyContent="flex-end"
+            height="100%"
+            spacing={2}
+          >
+            <Link exact href="/" underline="none">
+              <Typography variant="body1" color="white">
+                Home
+              </Typography>
+            </Link>
+            {!sessionUser ? (
+              <>
+                <Link href="/login" underline="none">
+                  <Typography variant="body1" color="white">
+                    Log In
+                  </Typography>
+                </Link>
+                <Link href="/signup" underline="none">
+                  <Typography variant="body1" color="white">
+                    Sign Up
+                  </Typography>
+                </Link>
+              </>
+            ) : (
+              <>
+                <Link href="/stack" underline="none">
+                  <Typography variant="body1" color="white">
+                    Stacks
+                  </Typography>
+                </Link>
+                <Link href="/brainfolio" underline="none">
+                  <Typography variant="body1" color="white">
+                    Brainfolio
+                  </Typography>
+                </Link>
+                <Link href="/profile" underline="none">
+                  <Button size="small" fullWidth variant="outlined">
+                    {sessionUser.username}
+                  </Button>
+                </Link>
+              </>
+            )}
+          </Stack>
+        </Grid>
       </Grid>
-      <Grid item xs={6}>
-        <Stack
-          direction="row"
-          alignItems="center"
-          justifyContent="flex-end"
-          height="100%"
-          spacing={2}
-        >
-          <Link exact href="/" underline="none">
-            <Typography variant="body1" color="white">
-              Home
-            </Typography>
-          </Link>
-          {!sessionUser ? (
-            <>
-              <Link href="/login" underline="none">
-                <Typography variant="body1" color="white">
-                  Log In
-                </Typography>
-              </Link>
-              <Link href="/signup" underline="none">
-                <Typography variant="body1" color="white">
-                  Sign Up
-                </Typography>
-              </Link>
-            </>
-          ) : (
-            <>
-              <Link href="/stack" underline="none">
-                <Typography variant="body1" color="white">
-                  Stacks
-                </Typography>
-              </Link>
-              <Link href="/brainfolio" underline="none">
-                <Typography variant="body1" color="white">
-                  Brainfolio
-                </Typography>
-              </Link>
-              <Link href="/profile" underline="none">
-                <Button size="small" fullWidth variant="outlined">
-                  {sessionUser.username}
-                </Button>
-              </Link>
-            </>
-          )}
-        </Stack>
-      </Grid>
-    </Grid>
+    </AppBar>
   );
 }
 
