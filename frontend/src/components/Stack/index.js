9b6bb5b0fa2c336fb716ab3248df1316d57f4454
mustafaomousa <mustafaomousa@icloud.com>
2022-01-01 19:28:28 -0600

    created stack menu

--- frontend/src/components/Stack/index.js
+++ frontend/src/components/Stack/index.js
@@ -8,11 +8,14 @@ import {
   IconButton,
   Typography,
   Stack as MuiStack,
+  Menu,
+  MenuItem,
 } from "@mui/material";
 import { makeStyles } from "@mui/styles";
 import MoreIcon from "@mui/icons-material/MoreHoriz";
 import PlayArrowOutlined from "@mui/icons-material/PlayArrowOutlined";
 import BookmarkAddOutlined from "@mui/icons-material/BookmarkAddOutlined";
+import { useState } from "react";
 
 const useStyles = makeStyles(() => ({
   root: {
@@ -22,6 +25,12 @@ const useStyles = makeStyles(() => ({
 
 const Stack = ({ stack }) => {
   const classes = useStyles();
+  const [stackEl, setStackEl] = useState(null);
+
+  const open = Boolean(stackEl);
+
+  const handleDropdownClick = (e) => setStackEl(e.currentTarget);
+  const handleDropdownClose = () => setStackEl(null);
 
   return (
     <Card className={classes.root} sx={{ backgroundColor: "#333333" }}>
@@ -32,9 +41,32 @@ const Stack = ({ stack }) => {
           </Typography>
         }
         action={
-          <IconButton sx={{ width: 40, height: 40 }}>
-            <MoreIcon sx={{ color: "white" }} />
-          </IconButton>
+          <>
+            <IconButton
+              id="stack-menu-button"
+              aria-controls={open ? "stack-menu" : undefined}
+              aria-haspopup="true"
+              aria-expanded={open ? "true" : undefined}
+              onClick={handleDropdownClick}
+              sx={{ width: 40, height: 40 }}
+            >
+              <MoreIcon sx={{ color: "white" }} />
+            </IconButton>
+            <Menu
+              id="stack-menu"
+              anchorEl={stackEl}
+              open={open}
+              onClose={handleDropdownClose}
+              anchorOrigin={{ vertical: "bottom", horizontal: "right" }}
+              transformOrigin={{ vertical: "top", horizontal: "right" }}
+              MenuListProps={{ sx: { minWidth: 200 } }}
+            >
+              <MenuItem>Play</MenuItem>
+              <MenuItem>Bookmark</MenuItem>
+              <MenuItem>Edit</MenuItem>
+              <MenuItem>Delete</MenuItem>
+            </Menu>
+          </>
         }
       />
       <Divider />
