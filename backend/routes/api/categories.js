5625db5f2f2c3d99127842db118cfcc39a0027a2
mustafaomousa <mustafaomousa@icloud.com>
2022-01-08 10:43:46 -0600

    updated categories route

--- backend/routes/api/categories.js
+++ backend/routes/api/categories.js
@@ -1,16 +1,17 @@
-const express = require('express');
-const { check } = require('express-validator');
-const asyncHandler = require('express-async-handler');
+const express = require("express");
+const asyncHandler = require("express-async-handler");
 
-const { handleValidationErrors } = require('../../utils/validation');
-const { Category } = require('../../db/models');
+const { Category } = require("../../db/models");
 
 const router = express.Router();
 
-router.get('/', asyncHandler(async (req, res) => {
+router.get(
+  "/",
+  asyncHandler(async (_req, res) => {
     const categories = await Category.findAll();
 
     return res.json({ categories });
-}));
+  })
+);
 
 module.exports = router;
